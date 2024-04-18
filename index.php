30014d98f54996b40d37a81f904c59cce3516ee5
Elizabeth980928 <66949528+Elizabeth980928@users.noreply.github.com>
2024-04-18 15:47:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<html lang="en"><head>
+    <!-- Required meta tags -->
+
+
+    <title>Rustenburg Local Municipality</title>
+
+    <?php include ('includes/inc_metadata.php')?>
+</head><body>
+
+
+<!-- navigation -->
+<?php// include ('includes/inc_header.php')?>
+<!-- nav part end -->
+
+<!-- content-->
+
+<?php include_once 'config/connection.php'; ?>
+<?php include_once 'functions/functions.php'; ?>
+<?php include ('pages/home.php')?>
+
+
+<!-- footer Start -->
+<?php //include ('includes/inc_footer.php')?>
+<?php include ('includes/inc_bottom.php')?>
+
+</body></html>
